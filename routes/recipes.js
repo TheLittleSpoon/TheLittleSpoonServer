cb7dc567bfb249f5bf6b9285bda0c2c0ca2c5f9b
itamar-marom <marom.itamar@gmail.com>
2021-02-28 23:00:07 +0200

    first try to delete only if it's your recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -34,7 +34,7 @@ router.post('/create', auth, async (req, res) => {
 })
 
 // Delete a recipe
-// Only the owner - TODO
+// Only the owner
 router.delete('/', auth, async (req, res) => {
     recipeId = _.pick(req.body, ['_id']);
     if (!recipeId) return res.status(400).send('Got no recipe ID to delete.');
@@ -43,6 +43,9 @@ router.delete('/', auth, async (req, res) => {
     if (!recipe) return res.status(400).send('Recipe does not exist.');
 
     // If this isn't this user's recipe, return denied.
+    // let author = recipe;
+    // console.log("user id: " + req.user._id);
+    // console.log("recipe id: " + (author));
     // if ((req.user._id) != (recipe.author._id)) return res.status(403).send('Access denied.');
 
     await Recipe.deleteOne({ _id: req.body._id });
