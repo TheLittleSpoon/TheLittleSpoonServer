8a7071469c46f590842ecb4c53dd6eb02cfe1bd8
Ilan Fleitman <ilan.fleitman@gmail.com>
2021-03-13 22:13:27 +0200

    add map reduce

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const { User, validate } = require('../models/user');
+const {User, validate} = require('../models/user');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
-const { Recipe } = require('../models/recipe');
+const {Recipe} = require('../models/recipe');
 const router = express.Router();
 
 // Get my details.
@@ -15,7 +15,7 @@ router.get('/me', auth, async (req, res) => {
 
 // Get all users:
 // Only Admin can get all the users.
-router.get('/', [ auth, admin ], async (req, res) => {
+router.get('/', [auth, admin], async (req, res) => {
     const users = await User.find().sort('name');
     res.send(users);
 });
@@ -23,10 +23,10 @@ router.get('/', [ auth, admin ], async (req, res) => {
 // Create a new user:
 // Anyone can create a user.
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
+    const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
+    let user = await User.findOne({email: req.body.email});
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
@@ -50,19 +50,19 @@ router.put('/', auth, async (req, res) => {
 
     if ((req.user._id != userId) && (!req.user.isAdmin)) return res.status(403).send('No access to this resource.');
 
-    let user = await User.findOne({ _id: userId });
+    let user = await User.findOne({_id: userId});
     if (!user) return res.status(400).send('User does not exist.');
 
-    let { name, password } = _.pick(req.body, ['name', 'password']);
+    let {name, password} = _.pick(req.body, ['name', 'password']);
     const salt = await bcrypt.genSalt(10);
     const finalPass = await bcrypt.hash(password, salt);
 
-    await User.updateOne({ _id: userId }, { 
+    await User.updateOne({_id: userId}, {
         name: name,
         password: finalPass
-    }, { omitUndefined: true });
+    }, {omitUndefined: true});
 
-    user = await User.findOne({ _id: userId });
+    user = await User.findOne({_id: userId});
 
     res.status(200).send(_.pick(user, ['_id', 'name', 'email']));
 });
@@ -72,31 +72,46 @@ router.put('/', auth, async (req, res) => {
 router.delete('/:id', auth, async (req, res) => {
     if ((req.user._id != req.params.id) && (!req.user.isAdmin)) return res.status(403).send('No access to this resource.');
 
-    let user = await User.findOne({ _id: req.params.id });
+    let user = await User.findOne({_id: req.params.id});
     if (!user) return res.status(400).send('User does not exist.');
 
-    await User.deleteOne({ _id: req.params.id });
+    await User.deleteOne({_id: req.params.id});
     res.send(user)
 });
 
 // Special Query
-router.post('/byFilter', [ auth, admin ], async (req, res) => {
-    let { name, recipeNumber, isAdmin } = _.pick(req.body, ['name', 'recipeNumber', 'isAdmin']);
+router.post('/byFilter', [auth, admin], async (req, res) => {
+    let {name, recipeNumber, isAdmin} = _.pick(req.body, ['name', 'recipeNumber', 'isAdmin']);
 
     let query = [];
 
-    if (name && (name != "")) query.push({"name": { $regex: name }});
+    if (name && (name != "")) query.push({"name": {$regex: name}});
     if (isAdmin) query.push({isAdmin: isAdmin});
-
-    let users = await User.find({ $and: query });
+    let users = [];
+    if (query.length !== 0) {
+        users = await User.find({$and: query});
+    } else {
+        users = await User.find();
+    }
     let finalUsers = [];
-    
-    await Promise.all(users.map(async (element) => {
-        let recipes = await Recipe.find({ author: element._id });
-        if (recipes && (recipes.length > recipeNumber)) finalUsers.push(element);
-    }));
-    
-    res.send(finalUsers);
+
+
+    if(parseInt(recipeNumber) < 0) {
+        res.send(users);
+        return;
+    }
+    const o = {};
+    o.map = 'function () { emit(this.author, this._id) }';
+    o.reduce = 'function (k, vals) { return vals.length }';
+
+    let wait = await Recipe.mapReduce(o, (err, model, stats) => {
+        model.results.forEach(value => {
+            if (value.value > parseInt(recipeNumber)) {
+                users.filter(value1 => value1._id.toString() === value._id.toString()).forEach(value1 => finalUsers.push(value1))
+                res.send(finalUsers);
+            }
+        })
+    })
 });
 
 module.exports = router;
